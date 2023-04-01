c32b456bc1b477fe48420f82eeb82cbe93ca0061
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-01 09:50:44 +0700

    improve test suite

--- src/counter.test.ts
+++ src/counter.test.ts
@@ -1,23 +1,39 @@
-import { increment, count, setCount } from "./counter";
 import { describe, test, expect } from "vitest";
 
-describe("count increment", () => {
+import { count, increment, decrement, setCount } from "./counter";
+
+describe("initial count value", () => {
   test("count is defined", () => {
     expect(count).toBeDefined();
     expect(count).toBe(0);
   });
+});
+
+describe("count increment", () => {
   test("count can be incremented with default value", () => {
     setCount(0);
     const newCount = increment(count);
     setCount(newCount);
-
     expect(count).toBe(1);
   });
+
   test("count can be incremented by 1", () => {
     setCount(0);
-    const newCount = increment(count, 1);
-    setCount(newCount);
-
+    setCount(increment(count, 1));
     expect(count).toBe(1);
+  });
+});
+
+describe("count decrement", () => {
+  test("count can be decremented with default value", () => {
+    setCount(0);
+    setCount(decrement(count));
+    expect(count).toBe(-1);
+  });
+
+  test("count can be decremented by 1", () => {
+    setCount(0);
+    setCount(decrement(count, 1));
+    expect(count).toBe(-1);
   });
 });
--- src/counter.ts
+++ src/counter.ts
@@ -4,6 +4,10 @@ export function increment(count: number, value = 1) {
   return count + value;
 }
 
+export function decrement(count: number, value = 1) {
+  return count - value;
+}
+
 export function setCount(nextCount: number) {
   count = nextCount;
 }
