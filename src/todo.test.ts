49e941db5811669c7bdbf372601fb2e2afa44930
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-08 09:04:52 +0700

    test: more todo items

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -5,10 +5,39 @@ describe("todo app", () => {
   test("get todos", () => {
     expect(getTodos([])).toStrictEqual([]);
   });
+
   test("add todo item", () => {
-    const todosData: Todo[] = [];
-    expect(addTodoItem(todosData, "running")).toStrictEqual([
+    let todosData: Todo[] = [];
+
+    const updatedTodos1 = addTodoItem(todosData, "running");
+    todosData = updatedTodos1;
+    expect(todosData).toStrictEqual([{ text: "running" }]);
+    expect(todosData[todosData.length - 1]).toStrictEqual({ text: "running" });
+
+    const updatedTodos2 = addTodoItem(todosData, "sleeping");
+    todosData = updatedTodos2;
+    expect(todosData).toStrictEqual([
       { text: "running" },
+      { text: "sleeping" },
     ]);
+    expect(todosData[todosData.length - 1]).toStrictEqual({ text: "sleeping" });
+
+    const updatedTodos3 = addTodoItem(todosData, "dreaming");
+    todosData = updatedTodos3;
+    expect(todosData[todosData.length - 1]).toStrictEqual({ text: "dreaming" });
+  });
+
+  test("add todo, then delete, then add again", () => {
+    let todosData: Todo[] = [];
+
+    const updatedTodos1 = addTodoItem(todosData, "cooking");
+    todosData = updatedTodos1;
+    expect(todosData).toStrictEqual([{ text: "cooking" }]);
+
+    // delete here
+
+    const updatedTodos3 = addTodoItem(todosData, "dieting");
+    todosData = updatedTodos3;
+    expect(todosData).toStrictEqual([{ text: "dieting" }]);
   });
 });
